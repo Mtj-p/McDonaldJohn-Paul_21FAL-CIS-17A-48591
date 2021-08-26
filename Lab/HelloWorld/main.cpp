07247601435738fcfecc8a0ba2b9eb22c8478552
jm2531972 <jay.mcdonald003@gmail.com>
2021-08-26 01:03:30 -0700

    Signed-off-by: jm2531972 <jay.mcdonald003@gmail.com>

---
+++ Lab/HelloWorld/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * file: HelloWorld
+ * Author: John-Paul McDonald
+ * Date: 8/26/2021
+ * Purpose: IntroLab
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+//Global Constants
+//Universal Math, Physics, Conversions, Higher Dimensions
+//Prototypes
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize Random Number Seed
+    
+    //Declare Variables
+    cout<<"Hello world";
+    //Initialize Variables
+    
+    //Process the inputs -> outputs
+    
+    //Display the results, verify inputs
+    
+    //Clean up and exit
+    return 0;
+}
