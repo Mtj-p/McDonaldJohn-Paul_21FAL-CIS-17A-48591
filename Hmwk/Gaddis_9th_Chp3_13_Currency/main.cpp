66be9562a4c357740da79c80948b54d9c72d5de2
jm2531972 <jay.mcdonald003@gmail.com>
2021-09-03 22:17:15 -0700

    Signed-off-by: jm2531972 <jay.mcdonald003@gmail.com>

--- Hmwk/Gaddis_9th_Chp3_13_Currency/main.cpp
+++ Hmwk/Gaddis_9th_Chp3_13_Currency/main.cpp
@@ -1,12 +1,13 @@
 /*
- * file: 
+ * file: Gaddis_9th_Ch3_13_Currency
  * Author: John-Paul McDonald
- * Date: 
- * Purpose: 
+ * Date: 09/03/2021
+ * Purpose: Convert USD to EUR and JPY
  */
 
 //System Libraries
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 //User Libraries
@@ -18,13 +19,20 @@ int main(int argc, char** argv) {
     //Initialize Random Number Seed
     
     //Declare Variables
-    cout<<"Hello world";
+    const float YEN_PER_DOLLAR = 98.93;
+    const float EUROS_PER_DOLLAR = .74;
+    float convCur = 0;
+    
     //Initialize Variables
     
     //Process the inputs -> outputs
+    cout<<"US to Yen and Euro calculator. Please input a US dollar amount:";
+    cin>>convCur;
     
     //Display the results, verify inputs
-    
+    cout<<endl;
+    cout.precision(2);
+    cout<<fixed<<setw(20)<<convCur*YEN_PER_DOLLAR<<" JPY \n"<<setw(20)<<convCur*EUROS_PER_DOLLAR<<" EUR"<<endl;
     //Clean up and exit
     return 0;
 }
